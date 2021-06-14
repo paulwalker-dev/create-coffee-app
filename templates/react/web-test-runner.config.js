dbd063b6d2de74416c7df883baa529e03f3aeece
Paul Walker <paulwalker@lavafallmc.net>
2021-06-14 17:18:53 -0400

    Fixed web test runner config

--- templates/react/web-test-runner.config.js
+++ templates/react/web-test-runner.config.js
@@ -1,5 +1,5 @@
 const vite = require("vite-web-test-runner-plugin");
-import { playwrightLauncher } from "@web/test-runner-playwright";
+const { playwrightLauncher } = require("@web/test-runner-playwright");
 
 module.exports = {
   plugins: [vite()],
